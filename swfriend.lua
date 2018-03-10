030694d7eaf29ce643f7a76d899eabd1edeb5aea
Rodrigo Leote <rodrigo.leote@poatek.com>
2018-03-10 16:26:51 -0600

    Fixed updating

--- swfriend.lua
+++ swfriend.lua
@@ -1,11 +1,13 @@
+----------Modules loading-------------
 localPath = scriptPath()
 toast ("Loading common lib")
 commonLib = loadstring(httpGet("https://raw.githubusercontent.com/AnkuLua/commonLib/master/commonLib.lua"))()
 toast ("Loading class lib")
 dofile(localPath .. "lib/class.lua")
 toast ("Loading updater")
--- Script auto-update
-local SWUpdater = require(localPath .. "autoupdater.lua")
+local SWUpdater = require(localPath .. "autoupdater")
+
+---------Script auto-update----------
 toast ("Executing update")
 currentVersion = dofile(localPath.."version.lua")
 SWUpdater.update(currentVersion)
