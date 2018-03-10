cd25e6574590f3ac0b1ad0196e3990dcbd353be5
Rodrigo Leote <rodrigo.leote@poatek.com>
2018-03-10 15:52:43 -0600

    Updater update 2

--- autoupdater.lua
+++ autoupdater.lua
@@ -1,18 +1,15 @@
-currentVersion = dofile(localPath.."version.lua")
-getNewestVersion = loadstring(httpGet("https://raw.githubusercontent.com/digaomatias/SWFriend/master/version.lua"))
-latestVersion = getNewestVersion()
-
-SWUpdater = class(function()
-                
-            end)    
+SWUpdater = class(function(u, currentVersion)
+    u.currentVersion = currentVersion
+end)    
 
 function SWUpdater:update()
-    if currentVersion == latestVersion then
+    local getNewestVersion = loadstring(httpGet("https://raw.githubusercontent.com/digaomatias/SWFriend/master/version.lua"))
+    local latestVersion = getNewestVersion()
+    if self.currentVersion == latestVersion then
         toast ("You are up to date!")
     else
         httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/version.lua", localPath .."version.lua")
-        httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/swfriend.lua", localPath .."Palbot.lua")
-        httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/imageupdater.lua", localPath .."imageupdater.lua")
+        httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/swfriend.lua", localPath .."swfriend.lua")
         scriptExit("SWFriend finished updating!")
     end
 end
