db56dd980401d754163780975ffccfa0645dfc7f
Rodrigo Leote <rodrigo.leote@poatek.com>
2018-03-10 14:57:59 -0600

    First attempt on creating an updater class.

--- autoupdater.lua
+++ autoupdater.lua
@@ -2,15 +2,17 @@ local currentVersion = dofile(localPath.."version.lua")
 local getNewestVersion = loadstring(httpGet("https://raw.githubusercontent.com/digaomatias/SWFriend/master/version.lua"))
 local latestVersion = getNewestVersion()
 
-function automaticUpdates ()
-    if autoUpdate == true then
-      if currentVersion == latestVersion then
+SWUpdater = class(function()
+                
+            end)    
+
+function SWUpdater:update()
+    if currentVersion == latestVersion then
         toast ("You are up to date!")
-      else
+    else
         httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/version.lua", localPath .."version.lua")
         httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/swfriend.lua", localPath .."Palbot.lua")
         httpDownload("https://raw.githubusercontent.com/digaomatias/SWFriend/master/imageupdater.lua", localPath .."imageupdater.lua")
         scriptExit("SWFriend finished updating!")
-      end
     end
-  end
+end
--- swfriend.lua
+++ swfriend.lua
@@ -1,5 +1,9 @@
 localPath = scriptPath()
-
+toast ("Loading common lib")
+commonLib = loadstring(httpGet("https://raw.githubusercontent.com/AnkuLua/commonLib/master/commonLib.lua"))()
+toast ("Loading updater")
 -- Script auto-update
 dofile(localPath .. "autoupdater.lua")
+toast ("Executing update")
+SWUpdater:update()
 
